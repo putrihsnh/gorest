4a7f2695dd88372586ef0a9b55e109ebaba0337c
putrihsnh <putrihasanah30@mhs.mdp.ac.id>
2022-11-24 09:40:07 +0700

    GORES

--- gorest/sript.js
+++ gorest/sript.js
@@ -1,15 +1,18 @@
-let listUser = document.getElementById("listUser")
+listUser = document.getElementById("listUser")
 let email = document.getElementById("email")
 let name = document.getElementById("name")
-let gander = document.getElementById("gender")
+let gender = document.getElementById("gender")
 let status = document.getElementById("status")
-
+let alert = document.getElementById("alert")
+let btnCreate = document.getElementById("btnCreate")
 getUser()
 
-
-
 function getUser() {
-    fetch("https://gorest.co.in/public/v2/users")
+    fetch("https://gorest.co.in/public/v2/users", {
+            headers: {
+                Authorization: "Bearer 577ea04fcea30fa6723a7ff6d939d0387b77d3add92b12dfd8e441c3a1d3ea9f"
+            }
+        })
         .then(response => response.json())
         .then(data => {
             console.log(data)
@@ -36,7 +39,7 @@ function deleteUser(id) {
     fetch("https://gorest.co.in/public/v2/users/" + id, {
             method: "DELETE",
             headers: {
-                Authorization:"Bearer 577ea04fcea30fa6723a7ff6d939d0387b77d3add92b12dfd8e441c3a1d3ea9f"
+                Authorization: "Bearer 577ea04fcea30fa6723a7ff6d939d0387b77d3add92b12dfd8e441c3a1d3ea9f"
             }
         })
         .then(response => {
@@ -48,35 +51,99 @@ function deleteUser(id) {
             console.log(error)
         })
 }
-function createUser () {
+
+function createUser(statusSimpan = 0, id = 0) {
+    if (statusSimpan == 0) {
+        //simpan data
+        console.log("Button simpan ditekan")
+        fetch("https://gorest.co.in/public/v2/users", {
+                method: "POST",
+                headers: {
+                    Authorization: "Bearer 577ea04fcea30fa6723a7ff6d939d0387b77d3add92b12dfd8e441c3a1d3ea9f"
+                },
+                body: JSON.stringify({
+                    "email": email.value,
+                    "name": name.value,
+                    "gender": gender.value,
+                    "status": status.value
+                })
+            })
+
+            .then(response => {
+                response.json()
+                console.log(response.status)
+            })
+            .then(result)
+        if (response.status == 201) { //user berhasil disimpan siserver Gorest
+            alert.innerHTML = '<div class = "alert alert-success" > User berhasil disimpan < /div>'
+        } else( // user gagal disimpan keserver gorest
+                alert.innerHTML = '<div class = "alert alert-danger" > User gagal disimpan < /div>'
+            )
+
+            .then(result => {
+                console.log(result)
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    } else {
+        //  ubah data
+        console.log("Button ubah ditekan")
+        fetch("https://gorest.co.in/public/v2/users/" + id, {
+                method: "PUT",
+                headers: {
+                    Authorization:"Bearer 577ea04fcea30fa6723a7ff6d939d0387b77d3add92b12dfd8e441c3a1d3ea9f"
+                },
+                body: JSON.stringify({
+                    "email": email.value,
+                    "name": name.value,
+                    "gender": gender.value,
+                    "status": status.value
+                })
+            })
+            .then(response => {
+                response.json()
+                console.log(response.status)
+            })
+            .then(result)
+        if (response.status == 200) { //user berhasil disimpan siserver Gorest
+            alert.innerHTML = '<div class = "alert alert-success" > User berhasil disimpan < /div>'
+        } else( // user gagal disimpan keserver gorest
+                alert.innerHTML = '<div class = "alert alert-danger" > User gagal disimpan < /div>'
+            )
+
+            .then(result => {
+                console.log(result)
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    }
+
+}
+
+function editUser(id) {
+    console.log(id)
     fetch("https://gorest.co.in/public/v2/users/" + id, {
-        method: "POST",
-        headers: {
-            'Content-Type' : 'application/json',
-            'Authorization':"Bearer 577ea04fcea30fa6723a7ff6d939d0387b77d3add92b12dfd8e441c3a1d3ea9f"
-        },
-        body: JSON.stringify({
-            "email" : email.value,
-            "name" : name.value,
-            "gender" : gender.value,
-            "status" : status.value
+            headers: {
+                Authorization: "Bearer 577ea04fcea30fa6723a7ff6d939d0387b77d3add92b12dfd8e441c3a1d3ea9f"
+            }
         })
-    })
-
-.then (Response => {
-    response.json()
-    console.log(response.status)
-})
-.then(result => {
-    console.log(result)
-   if(response.status == 201) {
-       alert.innerHTML = <div class= "alert-success">User Berhasil disimpan</div>
-   }else{
-       alert.innerHTML = <div class="alert alert-danger">user gagal disimpan</div>
-   }
-})
-
-.then(result => {
-    console.log(result)
-})
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            // set value input email, name, gender, dan status
+            email.value = data.email
+            name.value = data.name
+            gender.value = data.gender
+            status.value = data.status
+            // ubah teks "simpan" menjadi "ubah"
+            btnCreate.innerHTML = "Ubah"
+            // ubah onclick dengan menambahkan parameter "11"
+            // onclick="createUser(1)" 
+            btnCreate.setAttribute("onclick", "createUser(1" + id + ")")
+        })
+        .catch(error => {
+            console.log(error)
+        });
 }
